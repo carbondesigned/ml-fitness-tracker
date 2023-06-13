d7b267bd23ee2d2587c9f97e9a0c3d41c58c504d
Dylan <dylan.reeder5@gmail.com>
2023-06-12 20:15:18 -0400

    features

---
+++ src/features/build_features.py
@@ -0,0 +1,154 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from DataTransformation import LowPassFilter, PrincipalComponentAnalysis
+from TemporalAbstraction import NumericalAbstraction
+from FreqencyAbstraction import FourierTransformation
+
+
+# --------------------------------------------------------------
+# Load data
+# --------------------------------------------------------------
+
+df = pd.read_pickle("../../data/interim/02_outliers_removed_chauvenet.pkl")
+
+predictor_columns = list(df.columns[:6])
+
+# plot settings
+plt.style.use("fivethirtyeight")
+plt.rcParams["figure.figsize"] = (20, 5)
+plt.rcParams["figure.dpi"] = 100
+plt.rcParams["lines.linewidth"] = 2
+
+# --------------------------------------------------------------
+# Dealing with missing values (imputation)
+# --------------------------------------------------------------
+
+for col in predictor_columns:
+    
+    df[col] = df[col].interpolate()
+    
+df.info()
+
+# --------------------------------------------------------------
+# Calculating set duration
+# --------------------------------------------------------------
+
+for s in df["set"].unique():
+    start = df[df["set"] == s].index[0]
+    end = df[df["set"] == s].index[-1]
+    
+    duration = end - start
+    df.loc[(df["set"] == s), "duration"] = duration.seconds
+    
+duration_df = df.groupby(["category"])["duration"].mean()
+
+
+# --------------------------------------------------------------
+# Butterworth lowpass filter
+# --------------------------------------------------------------
+
+df_lowpass = df.copy()
+
+LowPass = LowPassFilter()
+
+fs = 1000 / 200
+
+cutoff = 1.2
+
+df_lowpass = LowPass.low_pass_filter(df_lowpass, "acc_y", fs, cutoff, order=5)
+subset = df_lowpass[df_lowpass["set"] == 45]
+
+subset[["acc_y_lowpass"]].plot()
+
+for col in predictor_columns:
+    df_lowpass = LowPass.low_pass_filter(df_lowpass, col, fs, cutoff, order=5)
+    df_lowpass[col] = df_lowpass[col + "_lowpass"]
+    del df_lowpass[col + "_lowpass"]
+# --------------------------------------------------------------
+# Principal component analysis PCA
+# --------------------------------------------------------------
+
+df_pca = df_lowpass.copy()
+PCA = PrincipalComponentAnalysis()
+
+pc_values = PCA.determine_pc_explained_variance(df_pca, predictor_columns)
+
+df_pca = PCA.apply_pca(df_pca, predictor_columns, 3)
+
+# --------------------------------------------------------------
+# Sum of squares attributes
+# --------------------------------------------------------------
+
+df_squared = df_pca.copy()
+acc_r = df_squared["acc_x"] ** 2 + df_squared["acc_y"] ** 2 + df_squared["acc_z"] ** 2
+gyro_r = df_squared["gyro_x"] ** 2 + df_squared["gyro_y"] ** 2 + df_squared["gyro_z"] ** 2
+
+df_squared["acc_r"] = np.sqrt(acc_r)
+df_squared["gyro_r"] = np.sqrt(gyro_r)
+
+# --------------------------------------------------------------
+# Temporal abstraction
+# --------------------------------------------------------------
+
+df_temporal = df_squared.copy()
+NumAbs = NumericalAbstraction()
+
+predictor_columns = predictor_columns + ["acc_r", "gyro_r"]
+
+# window size
+ws = int(1000 / 200)
+
+for col in predictor_columns:
+    df_temporal = NumAbs.abstract_numerical(df_temporal, [col], ws, "mean")
+    df_temporal = NumAbs.abstract_numerical(df_temporal, [col], ws, "std")
+
+df_temporal_list = []
+
+for s in df_temporal["set"].unique():
+    subset = df_temporal[df_temporal["set"] == s].copy()
+    for col in predictor_columns:
+        subset = NumAbs.abstract_numerical(subset, [col], ws, "mean")
+        subset = NumAbs.abstract_numerical(subset, [col], ws, "std")
+    df_temporal_list.append(subset)
+
+df_temporal = pd.concat(df_temporal_list)
+df_temporal.info()
+
+# --------------------------------------------------------------
+# Frequency features
+# --------------------------------------------------------------
+df_freq = df_temporal.copy().reset_index()
+FreqAbs = FourierTransformation()
+
+fs = int(1000 / 200)
+ws = int(2800 / 200)
+
+df_freq = FreqAbs.abstract_frequency(df_freq, ["acc_y"], ws, fs)
+
+df_freq_list = []
+
+for s in df_freq["set"].unique():
+    print (f"Applying Fourier transformations to set {s}")
+    subset = df_freq[df_freq["set"] == s].reset_index(drop=True).copy()
+    subset = FreqAbs.abstract_frequency(subset, predictor_columns, ws, fs)
+    df_freq_list.append(subset)
+    
+df_freq = pd.concat(df_freq_list).set_index("epoch (ms)", drop=True)
+
+
+
+# --------------------------------------------------------------
+# Dealing with overlapping windows
+# --------------------------------------------------------------
+
+
+
+# --------------------------------------------------------------
+# Clustering
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Export dataset
+# --------------------------------------------------------------
